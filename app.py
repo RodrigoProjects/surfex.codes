84327247781ba35d969c228accb99f2150b7f1af
RodrigoProjects <RodrigoProjects>
2020-02-08 19:36:29 +0000

    General Commit | 2020-02-08 19:36

--- app.py
+++ app.py
@@ -4,8 +4,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Hello World!'
+    return '<h1>Hello World!</h1>'
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(threaded=True,port=5000)
