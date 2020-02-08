8b8fff5a8ae94d40737d28cf4eb1653840ab323a
RodrigoProjects <RodrigoProjects>
2020-02-08 19:47:23 +0000

    General Commit | 2020-02-08 19:47

--- app.py
+++ app.py
@@ -7,6 +7,28 @@ app = Flask(__name__)
 def index():
     return '<h1>Hello World!!!</h1>'
 
+@app.route('/getmsg/', methods=['GET'])
+def respond():
+    # Retrieve the name from url parameter
+    name = request.args.get("name", None)
+
+    # For debugging
+    print(f"got name {name}")
+
+    response = {}
+
+    # Check if user sent a name at all
+    if not name:
+        response["ERROR"] = "no name found, please send a name."
+    # Check if the user entered a number not a name
+    elif str(name).isdigit():
+        response["ERROR"] = "name can't be numeric."
+    # Now the user entered a valid name
+    else:
+        response["MESSAGE"] = f"Welcome {name} to our awesome platform!!"
+
+    # Return the response in json format
+    return jsonify(response)
 
 
 
